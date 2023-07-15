633d4f0ed7efba7d0ccf8d6b0994e70848ca746d
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2023-07-15 14:53:27 +0100

    CharacterStack defined

---
+++ PasswordAuthenticator/src/CharacterStack.cs
@@ -0,0 +1,33 @@
+﻿namespace PasswordAuthenticator
+{
+    internal class CharacterStack
+    {
+        public void Push(char input)
+        {
+            length++;
+
+            char[] temp = new char[length];
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                temp[i] = password[i];
+            }
+
+            password = temp;
+            password[length - 1] = input;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public char GetChar(int input)
+        {
+            return password[input];
+        }
+
+        private int length;
+        private char[] password;
+    }
+}
--- PasswordAuthenticator/src/Implementation.cs
+++ PasswordAuthenticator/src/Implementation.cs
@@ -4,28 +4,17 @@
     {
         public Implementation()
         {
-            length = 0;
-            password = null;
+            password = new CharacterStack();
         }
 
         public override void Push(char input)
         {
-            length++;
-
-            char[] temp = new char[length];
-
-            for (int i = 0; i < length - 1; i++)
-            {
-                temp[i] = password[i];
-            }
-
-            password = temp;
-            password[length - 1] = input;
+            password.Push(input);
         }
 
         public override bool Length()
         {
-            if (length < 20)
+            if (password.GetLength() < 20)
             {
                 return false;
             }
@@ -37,11 +26,11 @@
         {
             char[] numerals = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < password.GetLength(); i++)
             {
                 for (int i2 = 0; i2 < 10; i2++)
                 {
-                    if (password[i] == numerals[i2])
+                    if (password.GetChar(i) == numerals[i2])
                     {
                         return true;
                     }
@@ -51,7 +40,6 @@
             return false;
         }
 
-        private int length;
-        private char[] password;
+        private readonly CharacterStack password;
     }
 }
