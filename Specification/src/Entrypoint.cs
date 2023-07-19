22b7f395239cf94078bbf0bb9ae1247023fc8217
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2023-07-19 15:38:04 +0100

    Entrypoint FieldTest defined

--- Specification/src/Entrypoint.cs
+++ Specification/src/Entrypoint.cs
@@ -5,7 +5,7 @@
         private static void Main()
         {
             System.Console.WriteLine(Results());
-            System.Console.ReadLine();
+            FieldTest();
         }
 
         private static string Results()
@@ -38,6 +38,50 @@
             output += new SupportedValidTest().Test();
 
             return output;
+        }
+
+        private static void FieldTest()
+        {
+            while (true)
+            {
+                PasswordAuthenticator.Facade fieldtest = new PasswordAuthenticator.Implementation();
+                string password = System.Console.ReadLine();
+
+                for (int i = 0; i < password.Length; i++)
+                {
+                    fieldtest.Push(password[i]);
+                }
+
+                if (!fieldtest.Length())
+                {
+                    System.Console.WriteLine("length: failed");
+                }
+
+                if (!fieldtest.Numerals())
+                {
+                    System.Console.WriteLine("numerals: failed");
+                }
+
+                if (!fieldtest.Symbols())
+                {
+                    System.Console.WriteLine("symbols: failed");
+                }
+
+                if (!fieldtest.Lowercase())
+                {
+                    System.Console.WriteLine("lowercase: failed");
+                }
+
+                if (!fieldtest.Uppercase())
+                {
+                    System.Console.WriteLine("uppercase: failed");
+                }
+
+                if (!fieldtest.Supported())
+                {
+                    System.Console.WriteLine("supported: failed");
+                }
+            }
         }
     }
 }
