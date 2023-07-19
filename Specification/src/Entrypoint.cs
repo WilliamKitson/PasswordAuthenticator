c9f524d4eccc4c56608b6b4dde31d2228ff1f148
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2023-07-19 16:21:04 +0100

    Entrypoint ExecuteFieldTest defined

--- Specification/src/Entrypoint.cs
+++ Specification/src/Entrypoint.cs
@@ -5,7 +5,11 @@
         private static void Main()
         {
             System.Console.WriteLine(new TestSuite().Test());
+            ExecuteFieldTest();
+        }
 
+        private static void ExecuteFieldTest()
+        {
             while (true)
             {
                 System.Console.WriteLine("please type a password:");
