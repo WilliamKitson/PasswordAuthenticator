8ecb160a8d53341d4fe684ad4da65faddf78db7c
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2023-07-19 15:41:15 +0100

    TestSuite defined

---
+++ Specification/src/TestSuite.cs
@@ -0,0 +1,37 @@
+﻿namespace Specification
+{
+    internal class TestSuite
+    {
+        public string Test()
+        {
+            string output = ConcatenateTests();
+
+            if (System.Convert.ToBoolean(output.Length))
+            {
+                return output;
+            }
+
+            return "all tests pass";
+        }
+
+        private string ConcatenateTests()
+        {
+            string output = "";
+
+            output += new LengthInvalidTest().Test();
+            output += new LengthValidTest().Test();
+            output += new NumeralsInvalidTest().Test();
+            output += new NumeralsValidTest().Test();
+            output += new SymbolsInvalidTest().Test();
+            output += new SymbolsValidTest().Test();
+            output += new LowercaseInvalidTest().Test();
+            output += new LowercaseValidTest().Test();
+            output += new UppercaseInvalidTest().Test();
+            output += new UppercaseValidTest().Test();
+            output += new SupportedInvalidTest().Test();
+            output += new SupportedValidTest().Test();
+
+            return output;
+        }
+    }
+}
