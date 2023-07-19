6f26bd9ab7b5c357bc21878b30908c1f63357144
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2023-07-19 15:43:52 +0100

    FieldTest defined

--- Specification/src/Entrypoint.cs
+++ Specification/src/Entrypoint.cs
@@ -5,51 +5,7 @@
         private static void Main()
         {
             System.Console.WriteLine(new TestSuite().Test());
-            FieldTest();
-        }
-
-        private static void FieldTest()
-        {
-            while (true)
-            {
-                PasswordAuthenticator.Facade fieldtest = new PasswordAuthenticator.Implementation();
-                string password = System.Console.ReadLine();
-
-                for (int i = 0; i < password.Length; i++)
-                {
-                    fieldtest.Push(password[i]);
-                }
-
-                if (!fieldtest.Length())
-                {
-                    System.Console.WriteLine("length: failed");
-                }
-
-                if (!fieldtest.Numerals())
-                {
-                    System.Console.WriteLine("numerals: failed");
-                }
-
-                if (!fieldtest.Symbols())
-                {
-                    System.Console.WriteLine("symbols: failed");
-                }
-
-                if (!fieldtest.Lowercase())
-                {
-                    System.Console.WriteLine("lowercase: failed");
-                }
-
-                if (!fieldtest.Uppercase())
-                {
-                    System.Console.WriteLine("uppercase: failed");
-                }
-
-                if (!fieldtest.Supported())
-                {
-                    System.Console.WriteLine("supported: failed");
-                }
-            }
+            new FieldTest().Test();
         }
     }
 }
---
+++ Specification/src/FieldTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Specification
+{
+    internal class FieldTest
+    {
+        public void Test()
+        {
+            while (true)
+            {
+                System.Console.WriteLine("please type a password:");
+                PasswordAuthenticator.Facade fieldtest = new PasswordAuthenticator.Implementation();
+                string password = System.Console.ReadLine();
+
+                for (int i = 0; i < password.Length; i++)
+                {
+                    fieldtest.Push(password[i]);
+                }
+
+                if (!fieldtest.Length())
+                {
+                    System.Console.WriteLine("length: failed");
+                }
+
+                if (!fieldtest.Numerals())
+                {
+                    System.Console.WriteLine("numerals: failed");
+                }
+
+                if (!fieldtest.Symbols())
+                {
+                    System.Console.WriteLine("symbols: failed");
+                }
+
+                if (!fieldtest.Lowercase())
+                {
+                    System.Console.WriteLine("lowercase: failed");
+                }
+
+                if (!fieldtest.Uppercase())
+                {
+                    System.Console.WriteLine("uppercase: failed");
+                }
+
+                if (!fieldtest.Supported())
+                {
+                    System.Console.WriteLine("supported: failed");
+                }
+            }
+        }
+    }
+}
